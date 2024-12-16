28190011bb1c307172a2357d73bd03cccaa14964
VincentVenan33 <92298629+VincentVenan33@users.noreply.github.com>
2024-12-17 00:08:46 +0700

    benerin dikit

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -47,7 +47,6 @@
           <i class="fa-solid fa-caret-down" style="font-size: 15px;"></i>
         </a>
         <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
-          <a class="dropdown-item mt-2"> Manage Accounts </a>
           <a href="{{ route('actionlogout') }}" class="dropdown-item"> Sign Out </a>
         </div>
       </li>
@@ -97,28 +96,9 @@
             <p class="profile-name">{{ Auth::user()->nama }} !</p>
             <div class="dropdown" data-display="static">
               <a href="#" class="nav-link d-flex user-switch-dropdown-toggler" id="UsersettingsDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-                <small class="designation text-muted">{{ Auth::user()->id_role }}</small>
+                <small class="designation text-muted">{{ Auth::user()->role->nama }}</small>
                 <span class="status-indicator online"></span>
               </a>
-              <div class="dropdown-menu" aria-labelledby="UsersettingsDropdown">
-                <a class="dropdown-item p-0">
-                  <div class="d-flex border-bottom">
-                    <div class="py-3 px-4 d-flex align-items-center justify-content-center">
-                      <i class="mdi mdi-bookmark-plus-outline mr-0 text-gray"></i>
-                    </div>
-                    <div class="py-3 px-4 d-flex align-items-center justify-content-center border-left border-right">
-                      <i class="mdi mdi-account-outline mr-0 text-gray"></i>
-                    </div>
-                    <div class="py-3 px-4 d-flex align-items-center justify-content-center">
-                      <i class="mdi mdi-alarm-check mr-0 text-gray"></i>
-                    </div>
-                  </div>
-                </a>
-                <a class="dropdown-item mt-2"> Manage Accounts </a>
-                <a class="dropdown-item"> Change Password </a>
-                <a class="dropdown-item"> Check Inbox </a>
-                <a class="dropdown-item"> Sign Out </a>
-              </div>
             </div>
           </div>
         </div>
